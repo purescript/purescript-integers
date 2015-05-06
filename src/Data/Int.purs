a6bcf4f52791915ad50cea5381d1a2882587c9bb
Harry Garrood <harry@garrood.me>
2015-05-06 15:53:54 +0100

    Add `odd`, `even`

--- src/Data/Int.purs
+++ src/Data/Int.purs
@@ -1,7 +1,11 @@
 module Data.Int
   ( fromNumber
   , toNumber
+  , even
+  , odd
   ) where
+
+import Data.Int.Bits
 
 -- | Creates an `Int` from a `Number` value. If the value is not already an
 -- | integer it is rounded towards zero (so both `0.9` and `-0.9` will become
@@ -21,3 +25,17 @@ foreign import toNumber
     return n;
   }
   """ :: Int -> Number
+
+-- | Returns whether an `Int` is an even number.
+-- |
+-- |     even 0 == true
+-- |     even 1 == false
+even :: Int -> Boolean
+even x = x .&. 1 == 0
+
+-- | The negation of `even`.
+-- |
+-- |     odd 0 == false
+-- |     odd 1 == false
+odd :: Int -> Boolean
+odd = not <<< even
