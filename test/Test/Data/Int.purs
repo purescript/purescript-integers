9b9ac702b99a6019d01d079fab0914422c072885
Gary Burgess <gary.burgess@gmail.com>
2015-05-10 14:45:31 +0100

    More descriptive and slightly better tests

--- test/Test/Data/Int.purs
+++ test/Test/Data/Int.purs
@@ -7,32 +7,54 @@ import Test.Assert (assert)
 
 testInt = do
 
-  log "testing fromNumber"
+  log "fromNumber should coerce integer values"
   assert $ fromNumber 1.0 == 1
   assert $ fromNumber 42.0 == 42
-  assert $ fromNumber 0.9 == 0
-  assert $ fromNumber (-0.9) == 0
   assert $ fromNumber 0.0 == 0
 
-  log "testing toNumber"
+  log "fromNumber should round floats towards zero"
+  assert $ fromNumber 0.9 == 0
+  assert $ fromNumber (-0.9) == 0
+
+  log "toNumber should coerce any integer to a number"
   assert $ toNumber 1 == 1.0
   assert $ toNumber (-1) == -1.0
   assert $ toNumber 0 == 0.0
 
-  log "testing fromString"
+  log "fromString should read integers"
   assert $ fromString "0" == Just 0
   assert $ fromString "9467" == Just 9467
+  assert $ fromString "-6" == Just (-6)
+
+  log "fromString should fail to read floats"
   assert $ fromString "0.1" == Nothing
+
+  log "fromString should fail to read integers outside of the int32 range"
   assert $ fromString "2147483648" == Nothing
+  assert $ fromString "-2147483649" == Nothing
 
-  log "testing even"
+  log "zero is even"
   assert $ even 0 == true
-  assert $ even 1 == false
-  assert $ even 2 == true
-  assert $ even 3 == false
 
-  log "testing odd"
+  log "even numbers are even"
+  assert $ even 2 == true
+  assert $ even 4 == true
+  assert $ even 100 == true
+
+  log "odd numbers are not even"
+  assert $ even 1 == false
+  assert $ even 3 == false
+  assert $ even 73 == false
+
+  log "zero is not odd"
   assert $ odd 0 == false
+
+  log "odd numbers are odd"
   assert $ odd 1 == true
-  assert $ odd 2 == false
   assert $ odd 3 == true
+  assert $ odd 73 == true
+
+  log "even numbers are not odd"
+  assert $ odd 2 == false
+  assert $ odd 4 == false
+  assert $ odd 100 == false
