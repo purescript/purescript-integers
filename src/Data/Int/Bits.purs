da54010cf25b7168e561ff8142b4a0f80c5d53ec
flip111 <flip101@gmail.com>
2023-10-08 23:54:05 +0200

    Add single bit functions

--- src/Data/Int/Bits.purs
+++ src/Data/Int/Bits.purs
@@ -7,6 +7,12 @@ module Data.Int.Bits
   , shr
   , zshr
   , complement
+  , zeroBits
+  , bit
+  , setBit
+  , clearBit
+  , complementBit
+  , testBit
   ) where
 
 -- | Bitwise AND.
@@ -35,3 +41,40 @@ foreign import zshr :: Int -> Int -> Int
 
 -- | Bitwise NOT.
 foreign import complement :: Int -> Int
+
+-- | zeroBits is the value with all bits unset.
+zeroBits :: Int
+zeroBits = 0
+
+-- | bit i is a value with the i'th bit set and all other bits clear.
+foreign import bit :: Int -> Int
+
+-- | Set a bit
+-- |
+-- | ```purescript
+-- | setBit 0 1 == 2
+-- | ```
+foreign import setBit :: Int -> Int -> Int
+
+-- | Clear a bit
+-- |
+-- | ```purescript
+-- | clearBit 7 1 == 5
+-- | ```
+foreign import clearBit :: Int -> Int -> Int 
+
+-- | XOR a bit
+-- |
+-- | ```purescript
+-- | complementBit 7 1 == 5
+-- | complementBit 0 1 == 2
+-- | ```
+foreign import complementBit :: Int -> Int -> Int 
+
+-- | Test a bit
+-- |
+-- | ```purescript
+-- | testBit 0 1 == false
+-- | testBit 7 1 == true
+-- | ```
+foreign import testBit :: Int -> Int -> Boolean
